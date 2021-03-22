d65aa364cfec1890ad17c7296f84d74a7a5db920
amancern3 <amandeep842.as@gmail.com>
2021-03-22 14:38:17 -0700

    added exit, pwd. clear works but needs ref

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,17 @@ use libc::{c_int, c_char};
 // for flushing 
 // ref: https://users.rust-lang.org/t/flush-the-standard-output-on-terminal/1018/2
 
-use std::io::Write; 
-use std::io::stdout; 
-use std::io::stdin;
+use std::io::{stdin, stdout, Write}; 
 
 // for accepting cmdline args
 // ref: https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html
 use std::env; 
 
-const CMDLINE_MAX: i32 = 512; 
+// crate to return current directory as PathBuf
+extern crate nix; 
+use nix::unistd;
+
+const CMDLINE_MAX: usize = 512; 
 
 /*
     A struct that holds (in order):
@@ -44,17 +46,53 @@ fn main() {
     let mut cmd: *mut i8; 
 
     loop {
-
+        
+        let mut s = String::new(); 
         let n1: Vec<c_char> ; 
         let mut ret: c_int ; 
 
         /* Print prompt */
-        println!("rusty$ ");
-        stdout().flush(); 
+        print!("rusty$ ");
+        let _ = stdout().flush(); 
 
-        /* Get command line */
-        libc::fgets(cmd, CMDLINE_MAX, );
-        
+        /* Get command line 
+            ref: https://users.rust-lang.org/t/how-to-get-user-input/5176/2
+        */
+        stdin().read_line(&mut s).expect("Command not recognized");
+        if s.len() > CMDLINE_MAX {
+            println!("Error: Command line max reached"); 
+            continue; 
+        }
+        if let Some('\n')=s.chars().next_back() {
+            s.pop();
+        }
+        if let Some('\r')=s.chars().next_back() {
+            s.pop();
+        }
+
+        /* Builtin Command */
+
+        /* Exit implementation*/
+        if s == "exit" || s == "Exit" || s == "EXIT"{
+            println!("exiting...");
+            println!("+ completed 'exit' [0]");
+            break;
+        } 
+        /* pwd implementation*/
+        if s == "pwd" {
+            let dir = unistd::getcwd().unwrap(); 
+            println!("{:?}", dir); 
+            println!("+ completed 'pwd' [0]");
+            continue;
+        }
+
+        /* clear implementation
+            ref:https://rosettacode.org/wiki/Terminal_control/Clear_the_screen#Rust
+        */
+        if s == "clear" {
+            print!("\x1B[2J");
+            println!("+ completed 'clear' [0]");
+        }
     }
 }
 
